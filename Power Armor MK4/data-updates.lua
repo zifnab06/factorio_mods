cad95f7da616ee71443f5521a37c5b06538fe6ef
Tom Powell <zifnab@zifnab06.net>
2017-05-29 13:27:04 -0700

    Fix compatibility with 0.1.0

---
+++ Power Armor MK4/data-updates.lua
@@ -0,0 +1,3 @@
+require("prototypes.item.power-armor-mk4")
+require("prototypes.recipe.power-armor-mk4")
+require("prototypes.technology.power-armor-mk4")
--- Power Armor MK4/data.lua
+++ Power Armor MK4/data.lua
@@ -1,4 +1,1 @@
-require("prototypes.item.power-armor-mk4")
 require("prototypes.equipment.equipment-grid2")
-require("prototypes.recipe.power-armor-mk4")
-require("prototypes.technology.power-armor-mk4")
